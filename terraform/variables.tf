eb8ad10bb89734d9c80c8240eaa22ccdc87c0404
Ervikson <65868221+Ervikson@users.noreply.github.com>
2025-11-25 20:14:56 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -24,6 +24,55 @@ variable "network_name" {
   default     = "final-vpc"
 }
 
+variable "sg_name" {
+  type        = string
+  default     = "final-app-sg"
+}
+
+variable "security_group_ingress" {
+  description = "Ingress rules for the application security group"
+  type = list(object({
+    description    = string
+    protocol       = string
+    port           = number
+    v4_cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      description    = "SSH"
+      protocol       = "TCP"
+      port           = 22
+      v4_cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      description    = "HTTP"
+      protocol       = "TCP"
+      port           = 80
+      v4_cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      description    = "HTTPS"
+      protocol       = "TCP"
+      port           = 443
+      v4_cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+variable "security_group_egress" {
+  description = "Egress rules for the application security group"
+  type = list(object({
+    protocol       = string
+    v4_cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      protocol       = "ANY"
+      v4_cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
 variable "subnets" {
   description = "Subnets per zone"
   type = map(object({
